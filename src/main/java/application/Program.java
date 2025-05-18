afcec6c69278f4bea6b07cbc5ffdb248bd6febff
Leandro Marques <leandrojerse@gmail.com>
2025-05-18 16:06:25 -0300

    Creting a SellerDao and DaoFactory

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.util.Date;
 
@@ -8,6 +11,9 @@ public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         Seller seller = new Seller(21,"bob", "bob@bob", new Date(), 1000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(department);
         System.out.println(seller);
     }
