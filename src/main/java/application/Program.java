db40eee517c18e1a1f1117389c0c6ea2a7871444
Leandro Marques <leandrojerse@gmail.com>
2025-05-18 15:46:52 -0300

    Creating Seller Entity

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
+        Seller seller = new Seller(21,"bob", "bob@bob", new Date(), 1000.0, department);
         System.out.println(department);
+        System.out.println(seller);
     }
 }
